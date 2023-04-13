54dbd7565328d8236f4f336a0117ed72754fe79e
MhmdRehan <mohamedrehan2486@gmail.com>
2023-04-13 12:49:01 +0530

    new codes

---
+++ src/org/demo/Example.java
@@ -0,0 +1,8 @@
+package org.demo;
+
+public class Example{
+	
+	
+	
+
+}
---
+++ src/org/demo/Sample.java
@@ -0,0 +1,29 @@
+package org.demo;
+
+import java.util.Scanner;
+
+public final class Sample{
+	
+	final int a=10;
+	
+
+	 private  void empId() {
+	
+		
+
+	}
+	 
+	 //by obj creation
+	 
+	 public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		 
+	}
+	
+	
+	
+	
+}
+
+
